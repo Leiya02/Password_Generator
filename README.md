39418344078c04e5812b47ebdd5cd413208c1fc7
Leiya Elizabeth English <leiyaelizabethenglish@Leiyas-MacBook-Pro.local>
2023-02-15 19:03:19 -0600

    Password Generator Final

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password_Generator
+
+I set up my variables to generate Button command, lower and uppercase letters, numbers and special characteristics
+
+I created a function to write a new password. Created a variable for "password" and gave it the value of "generatePassword()"
+
+I created password text, then I created a value as "password". Then I console.log "Enter Password" and " User Input.
+
+I created a function to generate a new password and set the critera of 8 between 128. 
+
+Then I confirmed the rest of the options for the User password. 
+
+MY PASSWORD GENERATOR BUTTON WAS WORKING SMOOTHLY, HOWEVER I CHANGED SOMETHING IN THE CODE AND COULD NOT GET THE ALERT (PROMPT) TO COME BACK. BUT OTHER THAN THAT CODE IS WRITTEN. COULD NOT FIND A WAY TO DEBUG "RETURN NULL"   
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCases = ["a", "b" , "c" , "d" , "e" , "f" , "g" , 
+"h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , 
+"r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"];
+var upperCases = ["A", "B" , "C" , "D" , "E" , "F" , "G" , 
+"H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , 
+"R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"];
+var number = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9];
+var specialChar = [ "!" , "*" , "@" , "#" , "$" , "&" ,
+"%" , "/" , "?" , "+" , "=" , "," , "." , "<" , ">"];
 
-// Write password to the #password input
+
+// My function to write a new password
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password")
+// passwordText.value = password;
+console.log("Enter Password")
+console.log("User Input" , password)
 }
 
+// function for generating new password
 function generatePassword() {
-  return "Hello World";
-}
+  var myLength = parseInt(prompt("How long is your password? Must be between 8 and 128"));
+};
+
+// password must be atleast 8 charaters and less than 128
+  if (myLength < 8 || myLength > 128) {
+    alert("does not meet criteria");
+
+// Ask user for numbers
+  confirm ("Do you want to use numbers?")    
+  //Ask user if their are any lowercase letters 
+  confirm ("Do you want to use lower case letters?")
+  //Ask user if their are any uppercase letters
+  confirm ("Do you want to use uppercase letters?")
+  //Ask user if their are any special characters
+  confirm ("Do you want to use special characters?")  
+  
+  return null
+  
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
